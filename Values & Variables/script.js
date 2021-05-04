de364aebd214a4db610acee6e9579590d04f699b
Claire <s3310507057@gmail.com>
2021-05-05 01:40:40 +0800

    Lecture one

---
+++ Values & Variables/script.js
@@ -0,0 +1,16 @@
+// my answer
+let country = 'Taichung'
+let continent = 'Asia'
+let population = 2300
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+// correct answer
+let country = 'Portugal';
+let continent = 'Europe';
+let population = 10;
+console.log(country);
+console.log(continent);
+console.log(population)
