84d6dd6a226040deefce1724073d48582f2844af
Claire <s3310507057@gmail.com>
2021-05-07 00:35:32 +0800

    2021/05/06

--- JSFondamentalsPartOne.js
+++ JSFondamentalsPartOne.js
@@ -1,5 +1,5 @@
 // Values & Variables
-const country = 'Taichung'
+const country = 'Taiwan'
 const continent = 'Asia'
 let population = 12
 
@@ -63,4 +63,77 @@ if (BMIMark > BMIJohn) {
   console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`)
 } else {
   console.log(`John's BMI ${BMIJohn} is higner than Mark's ${BMIMark}!`)
+}
+
+// Type Conversion and Coercion
+console.log('9' - '5') // 4
+console.log('19' - '13' + '17') // '617'
+console.log('19' - '13' + 17) // 23
+console.log('123' < 57) // false
+console.log(5 + 6 + '4' + 9 - 4 - 2) // 1143
+
+// Equality Operators: == v.s ===
+/*
+let numNeighbours = prompt('How many neighbour countries does your country have?')
+
+// Later, This helps us prevent bugs
+let numNeighbours = Number(prompt('How many neighbour countries does your country have?'))
+
+if (numNeighbours === 1) {
+  console.log('Only one border!')
+} else if (numNeighbours > 1) {
+  console.log('More than 1')
+} else {
+  console.log('No border')
+}*/
+
+// When use strict equality operator, then output will be 'No border!'. It's because prompt's value is string, but if/else condition is number! If I want to log 'Only one border!' successfully I have to convert numNeighbours' value type to number, use Number().
+
+// Logical Operator
+// Forgot to use Back-ticks
+if (language === 'English' && population < 50 && !isIsland) {
+  console.log(`You should live in ${country} :)!`)
+} else {
+  console.log(`${country} does not meet your criteria!:(`)
+}
+
+// Coding challenge #3
+const teamDolphins = (97 + 112 + 81) / 3
+const teamKoalas = (109 + 95 + 86) / 3
+
+// Data 1
+// if (teamDolphins > teamKoalas) {
+//   console.log('The winner is Team Dolphins!')
+// } else if (teamKoalas > teamDolphins) {
+//   console.log('The winner is Team Koalas!')
+// } else {
+//   console.log('Draw!')
+// }
+
+// Data Bonus 1 & 2
+if (teamDolphins > teamKoalas && teamDolphins >= 100) {
+  console.log('The winner is Team Dolphins!')
+} else if (teamKoalas > teamDolphins && teamKoalas >= 100) {
+  console.log('The winner is Team Koalas!')
+} else if (teamKoalas === teamDolphins && teamKoalas >= 100 && teamDolphins >= 100) {
+  console.log('Both teams win the trophy!')
+} else {
+  console.log('No one wins the trophy!')
+}
+
+// The switch state
+const day = 'monday'
+
+if (day === 'monday') {
+  console.log('Plan course structure.')
+} else if (day === 'tuesday') {
+  console.log('Prepare theory videos.')
+} else if (day === 'wednesday' || day === 'thursday') {
+  console.log('Write code example.')
+} else if (day === 'friday') {
+  console.log('Record videos')
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend!')
+} else {
+  console.log("It's not a valid day!")
 }
