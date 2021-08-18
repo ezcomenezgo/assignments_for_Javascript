cb0c80b67b7d23c5875fb2dfbc1a37dc0b675b7b
Claire <s3310507057@gmail.com>
2021-08-19 00:55:21 +0800

    20210819 created classes

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-08-16 09:58:21
- * @LastEditTime: 2021-08-17 10:02:27
+ * @LastEditTime: 2021-08-18 18:15:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /15-Mapty/starter/script.js
@@ -19,14 +19,61 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map, mapEvent
+class Workout {
+  date = new Date()
+  id = (Date.now() + '').slice(-10)
 
+  constructor(coords, distance, duration) {
+    this.distance = distance // in km
+    this.duration = duration // in min
+    this.coords = coords // [lat, lng]
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration)
+    this.cadence = cadence
+    this.clacPace()
+  }
+
+  clacPace() {
+    // min/km
+    this.pace = this.duration / this.distance
+    return this.pace
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevation) {
+    super(coords, distance, duration)
+    this.elevation = elevation
+    this.calcSpeed()
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60)
+    return this.speed
+  }
+}
+
+// const run1 = new Running([39, -12], 5.2, 24, 178)
+// const cyc1 = new Cycling([39, -12], 27, 99, 523)
+// console.log(run1, cyc1)
+
+///////////////////////////////////////
+// APPLICATION ARCHITECTURE 
 class App {
   #map
   #mapEvent
 
   constructor() {
     this._getPosition()
+
+    form.addEventListener('submit', this._newWorkout.bind(this))
+
+    inputType.addEventListener('change', this._toggleElevationField)
   }
 
   _getPosition() {
@@ -45,51 +92,48 @@ class App {
     const coords = [latitude, longitude]
 
     this.#map = L.map('map').setView(coords, 13);
-    const self = this
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.#map);
 
-    this.#map.on('click', function (e) {
-      self.#mapEvent = e
-
-      form.classList.remove('hidden')
-      inputDistance.focus()
-    })
+    this.#map.on('click', this._showForm.bind(this))
   }
 
 
-  _showHoem() { }
+  _showForm(e) {
+    this.#mapEvent = e
 
-  _toggleElevationField() { }
+    form.classList.remove('hidden')
+    inputDistance.focus()
+  }
 
-  _newWorkout() { }
+  _toggleElevationField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+  }
+
+  _newWorkout(e) {
+    e.preventDefault()
+
+    // Clear input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
+
+    const { lat, lng } = this.#mapEvent.latlng
+
+    L.marker([lat, lng]).addTo(this.#map)
+      .bindPopup(L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup'
+      }))
+      .setPopupContent('Workout')
+      .openPopup();
+  }
 }
 
 const app = new App()
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault()
 
-  // Clear input fields
-  inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = ''
-
-  const { lat, lng } = mapEvent.latlng
-
-  L.marker([lat, lng]).addTo(map)
-    .bindPopup(L.popup({
-      maxWidth: 250,
-      minWidth: 100,
-      autoClose: false,
-      closeOnClick: false,
-      className: 'running-popup'
-    }))
-    .setPopupContent('Workout')
-    .openPopup();
-})
-
-inputType.addEventListener('change', function () {
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
-})
